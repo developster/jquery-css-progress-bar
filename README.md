2c38adb7bda8f771d06f7e4ea22bc99a54b0dd38
Octavian Ciubotaru <octavian.ciubotaru@optimo.it>
2012-10-31 15:42:46 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-jquery-css-progress-bar
+JQuery CSS Progress Bar
 =======================
 
 A jquery plugin for Pure CSS Progress Bar by http://ivan.ly
+
+Check his project at: https://github.com/ivanvanderbyl/ui-progress-bar
+
+Usage
+=====
+
+Initialize:
+$("#myProgressBar").cssProgressBar();
+
+Update your progress:
+$("#myProgressBar").cssProgressBar("update", 99);
+
+Author
+======
+
+Ciubotaru Octavian
---
+++ jquery-css-progress-bar.js
@@ -0,0 +1,30 @@
+(function ($) {
+	
+	var methods = {
+		init : function (options) {
+			this.addClass("ui-progress-bar");
+			this.addClass("ui-container");
+			this.addClass("transition");
+			var progressDiv = document.createElement('div');
+			progressDiv.setAttribute("class", "ui-progress");
+			progressDiv.setAttribute("style", "width: 0%;");
+			this.append(progressDiv);
+			return this;
+		},
+		update : function (perc) {
+			this.children().css("width", perc + "%");
+			return this;
+		}
+	};
+	
+	$.fn.cssProgressBar = function (method) {
+	    if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' + method + ' does not exist on jQuery.cssProgressBar');
+		}    
+    };
+	
+})(jQuery);
